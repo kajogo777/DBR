d56134c57f96c9cb62b1419e8bc033a729fb4f8e
Michael Reda <michaelreda96@gmail.com>
2017-09-24 21:26:38 +0200

    all heroku related changes

--- heroku-web.js
+++ heroku-web.js
@@ -5,6 +5,6 @@ var morgan = require('morgan');
 var logger = morgan('combined');
 var fs = require('fs');
 var accessLogStream = fs.createWriteStream(__dirname + '/access.log', {flags: 'a'})
-app.use(morgan('combinedstream: accessLogStream'}));
+app.use(morgan({combinedstream: accessLogStream}));
 app.use(gzippo.staticGzip("" + __dirname + "/dist"));
 app.listen(process.env.PORT || 5000);
