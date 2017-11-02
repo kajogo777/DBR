d07bf181bcf68c7d689803898f212c5032c839c6
Michael Reda <michaelreda96@gmail.com>
2017-11-02 14:17:14 +0200

    rankings admin = true typecast bug fix

--- heroku-web.js
+++ heroku-web.js
@@ -455,7 +455,7 @@ app.post('/add_reading', function (req, res) {
 });
 
 app.post('/get_top_5_in_class', function (req, res) {
-    User.find({ "class": req.body.class, "admin": { $ne: "true" } }).sort({ "total_score": -1 }).limit(5).exec(function (err, users) {
+    User.find({ "class": req.body.class, "admin": { $gte: "5" } }).sort({ "total_score": -1 }).limit(5).exec(function (err, users) {
         if (err) {
             res.send(err);
         } else {
@@ -465,7 +465,7 @@ app.post('/get_top_5_in_class', function (req, res) {
 });
 
 app.post('/get_top_5', function (req, res) {
-    User.find({ "admin": { $ne: "true" } }).sort({ "total_score": -1 }).limit(5).exec(function (err, users) {
+    User.find({ "admin": { $gte: "5" } }).sort({ "total_score": -1 }).limit(5).exec(function (err, users) {
         if (err) {
             res.send(err);
         } else {
@@ -498,7 +498,7 @@ app.post('/add_users', function (req, res) {
 })
 
 app.post('/get_class_users', function (req, res) {
-    User.find({ "class": req.body.class, "admin": { $ne: "true" } }, function (err, users) {
+    User.find({ "class": req.body.class, "admin": { $gte: "5" } }, function (err, users) {
         if (err) {
             res.send(err);
         } else {
