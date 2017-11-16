e835348e6a6fd022469297e1a09af8f94015a1ca
Michael Reda <michaelreda96@gmail.com>
2017-11-17 00:42:35 +0200

    caching scripts

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -71,3 +71,18 @@ angular.module('yapp', [
         }
         
         angular.module('yapp').config(['$provide', configureTemplateFactory]);
+
+
+        angular.module('yapp').config(['$httpProvider', function($httpProvider) {
+            //initialize get if not there
+            if (!$httpProvider.defaults.headers.get) {
+                $httpProvider.defaults.headers.get = {};    
+            }    
+
+        
+            //disable IE ajax request caching
+            $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
+            // extra
+            $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
+            $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+        }]);
