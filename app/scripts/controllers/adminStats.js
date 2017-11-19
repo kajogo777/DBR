ec242a829d180369cfb61d693c0b81df9543ec63
Michael Reda <michaelreda96@gmail.com>
2017-11-20 00:43:31 +0200

    days statistics

--- app/scripts/controllers/adminStats.js
+++ app/scripts/controllers/adminStats.js
@@ -15,6 +15,7 @@ angular.module('yapp')
       $scope.readings=[];
       $scope.trophies=[];
       $scope.weeks=[];
+      $scope.days=[];
 
       $http.get($window.localStorage.getItem("base_url")+"/get_readings").then(function(response){
         for(var i=0;i<Math.ceil(response.data.length/7);i++){
@@ -29,6 +30,30 @@ angular.module('yapp')
 
       $http.get($window.localStorage.getItem("base_url")+"/get_levels").then(function(response){
         $scope.levels= response.data;
+      });
+
+      $http.get($window.localStorage.getItem("base_url")+"/get_reading_dates").then(function(response){
+        var users = response.data;
+        var days_array = [];
+        for(var u=0;u<users.length;u++){
+          var reading_date_of_user = users[u].reading_dates;
+          for(var d=0;d<reading_date_of_user.length;d++){
+            var date = new Date(reading_date_of_user[d]);
+            date = date.getDate()+"/"+(date.getMonth()+1)+"/"+date.getFullYear();
+            var date_index= days_array.indexOf(date);
+            if(date_index<0){
+              days_array.push(date);
+              $scope.days.push({
+                date: date,
+                users_counter: 1
+              });
+            }else{
+              $scope.days[date_index].users_counter++;
+            }
+          }
+        }
+        $scope.days.sort(function(a,b) {return (a.date > b.date) ? 1 :0});
+        console.log($scope.days);
       });
 
 
