754f99f5b5d9ebb3819fd06862307532a212d9b7
Michael Reda <michaelreda96@gmail.com>
2017-11-20 00:22:14 +0200

    classifying readings according to weeks

--- app/scripts/controllers/adminStats.js
+++ app/scripts/controllers/adminStats.js
@@ -14,8 +14,12 @@ angular.module('yapp')
       var user=JSON.parse($window.localStorage.getItem("user"));
       $scope.readings=[];
       $scope.trophies=[];
+      $scope.weeks=[];
 
       $http.get($window.localStorage.getItem("base_url")+"/get_readings").then(function(response){
+        for(var i=0;i<Math.ceil(response.data.length/7);i++){
+          $scope.weeks.push(i);
+        }
         $scope.readings= response.data;
       });
 
