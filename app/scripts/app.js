040be7c5e9b068d1489b73c836225fcb49feaa6e
Michael Reda <michaelreda96@gmail.com>
2017-11-22 07:07:22 +0200

    time stamp hashing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -47,3 +47,41 @@ angular.module('yapp', [
             });
         });
   
+//disable caching 
+function configureTemplateFactory($provide) { 
+    // Set a suffix outside the decorator function  
+    var cacheBuster = Date.now().toString(); 
+ 
+    function templateFactoryDecorator($delegate) { 
+        var fromUrl = angular.bind($delegate, $delegate.fromUrl); 
+        $delegate.fromUrl = function (url, params) { 
+            if (url !== null && angular.isDefined(url) && angular.isString(url)) { 
+                url += (url.indexOf("?") === -1 ? "?" : "&"); 
+                url += "v=" + cacheBuster; 
+            } 
+ 
+            return fromUrl(url, params); 
+        }; 
+ 
+        return $delegate; 
+    } 
+ 
+    $provide.decorator('$templateFactory', ['$delegate', templateFactoryDecorator]); 
+} 
+ 
+angular.module('yapp').config(['$provide', configureTemplateFactory]); 
+
+
+angular.module('yapp').config(['$httpProvider', function($httpProvider) { 
+    //initialize get if not there 
+    if (!$httpProvider.defaults.headers.get) { 
+        $httpProvider.defaults.headers.get = {};     
+    }     
+
+ 
+    //disable IE ajax request caching 
+    $httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT'; 
+    // extra 
+    $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache'; 
+    $httpProvider.defaults.headers.get['Pragma'] = 'no-cache'; 
+}]);
