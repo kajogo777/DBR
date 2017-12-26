8e5f6b658305acbc7d84160a8a279accb07c4538
Michael Reda <michaelreda96@gmail.com>
2017-12-26 11:39:58 +0200

    base url

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -8,34 +8,32 @@
  * Controller of yapp
  */
 angular.module('yapp')
-  .controller('LoginCtrl', function($scope, $location,$http,$window) {
-    //setting base address
-    if($location.absUrl().includes("localhost")){
-      $window.localStorage.setItem("base_url","http://localhost:5000");
-    }else if($location.absUrl().includes("dbrtest")){
-       $window.localStorage.setItem("base_url","https://dbrtest.herokuapp.com");
-   }else{
-      $window.localStorage.setItem("base_url","https://dbr.herokuapp.com");
+  .controller('LoginCtrl', function ($scope, $location, $http, $window) {
+    //set base url
+    $window.localStorage.setItem("base_url", new $window.URL($location.absUrl()).origin);
+
+    if ($location.absUrl().includes("localhost")) {
+      $window.localStorage.setItem("base_url", "http://localhost:5000");
     }
-    
-    if(localStorage.getItem("user")!= undefined){
+
+    if (localStorage.getItem("user") != undefined) {
       $location.path('/dashboard');
     }
-    
-    $scope.login_failed=undefined;
-    $scope.submit = function(username,password) {
-      $http.post($window.localStorage.getItem("base_url")+"/login",{"username":username,"password":password}).then(function(response){
+
+    $scope.login_failed = undefined;
+    $scope.submit = function (username, password) {
+      $http.post($window.localStorage.getItem("base_url") + "/login", { "username": username, "password": password }).then(function (response) {
         console.log(response.data);
-        if(response.status==200){
-          $window.localStorage.setItem("user",JSON.stringify(response.data));
+        if (response.status == 200) {
+          $window.localStorage.setItem("user", JSON.stringify(response.data));
           $location.path('/dashboard');
-        }else if(response.status==201){
+        } else if (response.status == 201) {
           console.log(response.data);
-          $scope.login_failed=response.data;
+          $scope.login_failed = response.data;
         }
-        
+
       });
-      
+
 
       return false;
     }
